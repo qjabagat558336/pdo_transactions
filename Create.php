f51ed1894eaed66332d5c9ca517d7d60d9453bda
qjabagat558336 <q.jabagat.558336@umindanao.edu.ph>
2026-03-04 00:05:00 +0800

    Create Create.php

---
+++ Create.php
@@ -0,0 +1,30 @@
+<?php
+require 'db.php';
+require 'functions.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $item = $_POST['item'];
+    $price = $_POST['price'];
+    $qty = $_POST['qty'];
+
+    
+    if (!validateNumber($price) || !validateNumber($qty)) {
+        die("Price and Quantity must be non-negative.");
+    }
+
+    $total = computeTotal($price, $qty);
+
+    $sql = "INSERT INTO transactions (item, price, qty, total)
+            VALUES (:item, :price, :qty, :total)";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        ':item' => $item,
+        ':price' => $price,
+        ':qty' => $qty,
+        ':total' => $total
+    ]);
+
+    redirect("read.php");
+}
+?>
